bb64dd613836162a0c814e5cd27693f19ed0b17b
alessiofiorito <alessio.fiorito@outlook.com>
2024-11-11 01:30:53 +0100

    Game with basic AI (no minimax yet)

--- functions.h
+++ functions.h
@@ -3,7 +3,11 @@
 #include <array>
 
 void draw_grid(const std::array<char,9>& grid);
-
+bool is_position_ok(int player_position, const std::array<char,9>& grid);
+int get_player_position();
+bool has_someone_won(const std::array<char,9>& grid);
+bool is_draw(const std::array<char,9>& grid);
+void ai_turn(std::array<char,9>& grid);
 
 
 #endif FUNCTIONS.H
